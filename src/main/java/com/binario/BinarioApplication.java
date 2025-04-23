c0010a3e5828b1c437dacf6e3d8bf15e25920db5
kgaraev_3@mail.ru <kgaraev_3@mail.ru>
2025-04-23 10:25:58 +0300

    init commit

---
+++ src/main/java/com/binario/BinarioApplication.java
@@ -0,0 +1,11 @@
+package com.binario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BinarioApplication.class, args);
+    }
+}
---
+++ src/test/java/com/binario/BinarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
